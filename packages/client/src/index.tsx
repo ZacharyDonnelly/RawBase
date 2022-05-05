c34acc6ac1e90136d9194ec342b8737bbf6147e4
zdonnelly-gavant <80919132+zdonnelly-gavant@users.noreply.github.com>
2022-05-05 16:01:01 -0400

    chore: swapping deps to package

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -3,9 +3,10 @@ import { Provider } from 'react-redux'
 
 import { BrowserRouter } from 'react-router-dom'
 
+import { store } from '../store'
+
 import AppRouter from './routes'
 
-import { store } from './store/store'
 import './index.scss'
 
 const container = document.getElementById('root')!
