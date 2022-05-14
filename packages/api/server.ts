24e0acd8d2b39d29708dbb7c68e8b3bf7bbe61c3
zdonnelly-gavant <80919132+zdonnelly-gavant@users.noreply.github.com>
2022-05-13 22:24:44 -0400

    chore: typescript config:

--- packages/api/server.ts
+++ packages/api/server.ts
@@ -1,15 +1,39 @@
-import { ApolloServer } from 'apollo-server'
+import express, { urlencoded } from 'express'
+import { ApolloServer } from 'apollo-server-express'
 import { schema } from './src/graphql'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
 
 const db = {}
+
+const app = express()
 
 const server = new ApolloServer({
   schema,
   introspection: true,
+  csrfPrevention: true,
   context: { db },
 })
 
+const configureApp = () => {
+  app.disable('x-powered-by')
+  app.use(cors({ credentials: true }))
+  app.use(cookieParser(process.env.COOKIE_SECRET))
+  app.use(morgan('dev'))
+  app.use(urlencoded({ extended: true }))
+  app.use(express.static('public'))
+}
+
+const startServer = async () => {
+  await server.start()
+  server.applyMiddleware({ app })
+}
+
+configureApp()
+startServer()
+
 // once sequelize is up & running, start market data service http listener
-server.listen(4000).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
+app.listen({ port: 3006 }, () => {
+  console.log(`🚀 Server ready at 127.0.0.1:3006`)
 })
