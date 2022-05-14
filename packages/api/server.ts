e3b569026eb2483f3eb9fe1d454ccbcfb3be570b
zdonnelly-gavant <80919132+zdonnelly-gavant@users.noreply.github.com>
2022-05-14 18:31:34 -0400

    feat: better server config

--- packages/api/server.ts
+++ packages/api/server.ts
@@ -1,4 +1,5 @@
 import express, { urlencoded } from 'express'
+
 import { ApolloServer } from 'apollo-server-express'
 
 import cors from 'cors'
@@ -32,10 +33,23 @@ const startServer = async () => {
   server.applyMiddleware({ app, path: '/graphql' })
 }
 
-configureApp()
-startServer()
+async function main() {
+  configureApp()
+  await startServer()
 
-// once sequelize is up & running, start market data service http listener
-app.listen({ port: 3006 }, () => {
-  console.log(`🚀 Server ready at 127.0.0.1:3006`)
+  await new Promise<void>((resolve) =>
+    app.listen({ port: 3006 }, () => {
+      console.log(
+        [
+          `GraphQL server ready at \thttp://localhost:3006${server.graphqlPath}\n`,
+          `API ready on \thttp://localhost:3006${''}\t`
+        ].join('')
+      )
+      resolve()
+    })
+  )
+}
+
+main().catch((err) => {
+  console.error(err)
 })
