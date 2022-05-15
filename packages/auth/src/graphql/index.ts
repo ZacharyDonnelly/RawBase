212b1ba8a855a741e04fce5e4859ac9f0d17493e
zdonnelly-gavant <80919132+zdonnelly-gavant@users.noreply.github.com>
2022-05-14 20:24:03 -0400

    chore: add db context to apollo server

--- packages/auth/src/graphql/index.ts
+++ packages/auth/src/graphql/index.ts
@@ -8,6 +8,8 @@ import { ApolloServer, ExpressContext } from 'apollo-server-express'
 
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
 
+import db from '../db'
+
 import resolvers from './resolvers'
 
 export const schema: any = makeExecutableSchema({
@@ -22,6 +24,7 @@ export async function createApolloServer(
 ): Promise<ApolloServer<ExpressContext>> {
   const server = new ApolloServer({
     schema,
+    context: () => ({ db }),
     introspection: true,
     csrfPrevention: true,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
