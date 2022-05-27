caead2bbbfe3305a845551d3b89cd312f7176cda
zdonnelly-gavant <80919132+zdonnelly-gavant@users.noreply.github.com>
2022-05-26 21:33:40 -0400

    chore: updates to release config

--- release.config.js
+++ release.config.js
@@ -1,42 +1,41 @@
 module.exports = {
-    plugins: [
-      [
-        "@semantic-release/commit-analyzer",
-        {
-          preset: "conventionalcommits",
-          releaseRules: [
-            { type: "docs", release: "patch" },
-            { type: "perf", release: "patch" },
-            { type: "style", release: "patch" },
-          ],
-          parserOpts: {
-            noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
-          },
-        },
-      ],
-      [
-        "@semantic-release/release-notes-generator",
-        {
-          preset: "conventionalcommits",
-          presetConfig: {
-            types: [
-              { type: "feat", section: "Features" },
-              { type: "fix", section: "Bug Fixes" },
-              { type: "chore", section: "Other" },
-              { type: "docs", section: "Documentation" },
-              { type: "style", section: "Other" },
-              { type: "refactor", section: "Other" },
-              { type: "perf", section: "Other" },
-              { type: "test", section: "Other" },
-              { type: "build", section: "Other" },
-              { type: "ci", section: "Other" },
-            ],
-          },
-        },
-      ],
-      "@semantic-release/changelog",
-      "@semantic-release/github",
-      "@semantic-release/npm",
-      "@semantic-release/git",
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { type: 'docs', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'style', release: 'patch' }
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+        }
+      }
     ],
-  }
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'chore', section: 'Other' },
+            { type: 'sec', section: 'Security' },
+            { type: 'style', section: 'Other' },
+            { type: 'refactor', section: 'Other' },
+            { type: 'perf', section: 'Other' },
+            { type: 'test', section: 'Other' },
+            { type: 'build', section: 'Other' },
+            { type: 'ci', section: 'Other' }
+          ]
+        }
+      }
+    ],
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    '@semantic-release/git'
+  ]
+}
