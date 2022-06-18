cefe0dd7c32b0c45dd7f31eb5f74539ecf4c9303
zdonnelly-gavant <80919132+zdonnelly-gavant@users.noreply.github.com>
2022-06-18 16:37:17 -0400

    fix: revert back to yarn v1 and fixed ts issues

--- packages/auth/server.ts
+++ packages/auth/server.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import { createServer } from 'http'
 
-import chalk from 'chalk'
+// import chalk from 'chalk'
 
 // ts-ignore
 import cookieParser from 'cookie-parser'
@@ -17,53 +17,47 @@ const app = express()
 const httpServer = createServer(app)
 
 const configureApp = () => {
-  app.disable('x-powered-by')
-  app.use(cors({ credentials: true }))
-  app.use(cookieParser(process.env.COOKIE_SECRET))
-  app.use(morgan('dev'))
-  app.use(urlencoded({ extended: true }))
+    app.disable('x-powered-by')
+    app.use(cors({ credentials: true }))
+    app.use(cookieParser(process.env.COOKIE_SECRET))
+    app.use(morgan('dev'))
+    app.use(urlencoded({ extended: true }))
 }
 
 const main = async () => {
-  dotenv.config()
-  configureApp()
-  await createApolloServer(db, httpServer, app)
+    dotenv.config()
+    configureApp()
+    await createApolloServer(db, httpServer, app)
 
-  // * Testing SQLITE DB connection on dev
-  if (process.env.NODE_ENV === 'development') {
-    try {
-      await sequelizeConnection.authenticate()
-      console.log('Connection has been established successfully.')
-    } catch (error) {
-      console.error('Unable to connect to the database:', error)
+    // * Testing SQLITE DB connection on dev
+    if (process.env.NODE_ENV === 'development') {
+        try {
+            await sequelizeConnection.authenticate()
+            console.log('Connection has been established successfully.')
+        } catch (error) {
+            console.error('Unable to connect to the database:', error)
+        }
     }
-  }
 
-  await new Promise<void>((resolve) =>
-    sequelizeConnection.sync().then(() => {
-      app.listen({ port: process.env.PORT }, () => {
-        console.log(
-          [
-            `\n`,
-            chalk?.bgBlueBright.white.bold(
-              `GraphQL server ${''} ready at\thttp://localhost:${
-                process.env.PORT
-              }/graphql\n`
-            ),
-            chalk?.bgWhite.black(
-              `Rest API server ready at\thttp://localhost:${process.env.PORT}\n`
-            ),
-            chalk?.bgMagentaBright.black(
-              `Database server ready at\tsqlite://127.0.0.1:${process.env.DB_PORT}\n`
-            )
-          ].join('')
-        )
-        resolve()
-      })
-    })
-  )
+    await new Promise<void>((resolve) =>
+        sequelizeConnection.sync().then(() => {
+            app.listen({ port: process.env.PORT }, () => {
+                console.log(
+                    [
+                        `\n`,
+                        `GraphQL server ${''} ready at\thttp://localhost:${
+                            process.env.PORT
+                        }/graphql\n`,
+                        `Rest API server ready at\thttp://localhost:${process.env.PORT}\n`,
+                        `Database server ready at\tsqlite://127.0.0.1:${process.env.DB_PORT}\n`
+                    ].join('')
+                )
+                resolve()
+            })
+        })
+    )
 }
 
 main().catch((err) => {
-  console.error(err)
+    console.error(err)
 })
